bdeaae221c5e92fbfd2406000be29002e2fe7291
Rizwan200-0 <differentpurpose44@gmail.com>
2025-04-28 13:30:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def index():
 
 if __name__ == "__main__":
     port = os.environ.get('PORT', 5000)  # Get the port from Render environment
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=True)  # Added threaded=True
